8707f91a8b8c6a3b5fb10b273c2fbd0cb28d6ea9
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2021-11-19 16:31:57 -0500

    Use setuptools.find_packages() so submodules are also packaged, exclude test

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="expyre-wfl",
     version="0.1.0b",
-    packages=["expyre"],
+    packages=setuptools.find_packages(exclude=["tests"]),
     install_requires=["click>=7.0"],
     entry_points="""
     [console_scripts]
