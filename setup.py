3fea886aac399e0db132c4247aeaa66805f423a1
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2024-05-16 14:07:59 -0400

    Add long_description to setup.py from README.md

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import setuptools
 setuptools.setup(
     name="expyre-wfl",
     version="0.1.2",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(exclude=["tests"]),
     install_requires=["click>=7.0"],
     entry_points="""
