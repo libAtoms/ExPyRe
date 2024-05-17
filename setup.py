1621003a4ee7ffa435b091d5b4e19a5aa9cd3750
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2024-05-17 18:12:57 -0400

    add license to setup.py and bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import setuptools
 
 setuptools.setup(
     name="expyre-wfl",
-    version="0.1.3",
+    version="0.1.4",
+    license_files=["LICENSE"],
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(exclude=["tests"]),
